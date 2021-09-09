0218e627a6669a8ac92f22a6739b5a934ead4c4e
shiguangruyu <1936409824@qq.com>
2021-09-09 21:28:37 +0800

    ui系统完成，还有部分bug

--- Assets/_VIP/AI/MyUnitAI.cs
+++ Assets/_VIP/AI/MyUnitAI.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class MyUnitAI : MyAIBase
 {
-    public GameObject projectilePrefab;
+    //public GameObject projectilePrefab;
+    public AssetReference projectilePrefab;//addrressables系統引用對象不使用gameobject
     public Transform firePos;
 
     public void OnDealDamage()
@@ -18,19 +20,22 @@ public class MyUnitAI : MyAIBase
         if (this.target.GetComponent<MyPlaceableView>().data.hitPoints < 0)
         {
             this.target.GetComponent<MyPlaceableView>().data.hitPoints = 0;
+
+            MyPlaceableMgr.instance.OnEnterDie(this.target);
+
             this.target = null;
         }
     }
 
-    public void OnFireProjectile()
+    public async void OnFireProjectile()
     {
         //实例化法师火球
-        GameObject go = Instantiate(
+        GameObject go = await Addressables.InstantiateAsync(
             projectilePrefab,
             firePos.position,
             Quaternion.identity,
             MyProjectileMgr.instance.transform
-            );//把投掷物放在手部位置，但是不是以手为父节点，不跟手移动
+            ).Task;//把投掷物放在手部位置，但是不是以手为父节点，不跟手移动
         //设置投掷物的释放者
         go.GetComponent<MyProjectile>().caster = this;
         go.GetComponent<MyProjectile>().target = this.target;
